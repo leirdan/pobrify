d9e49ac13b5aecad57ff37387798dff708ba428f
leirdan <uouleirdan@gmail.com>
2023-06-27 09:12:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pobrify
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string main = "spotify.com/user/leirdan";
+            string[] aux = main.Split('/');
+            Console.WriteLine(aux[2]);
+            // "leirdan"
+
+            Console.ReadLine();
+        }
+    }
+}
